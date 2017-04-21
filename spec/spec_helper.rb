01dad25ac4d4041c876be0d0b39ccf24f1859cda
dayvsonlima <dayvsonlima31@gmail.com>
2017-04-20 23:11:19 -0300

    Config table creation in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,20 @@
 require 'rspec'
+require 'active_record'
 require 'give-record'
+
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: ':memory:'
+)
+
+RSpec.configure do |config|
+  config.before(:each) do
+    ActiveRecord::Base.connection.create_table :mock_table do |t|
+      t.timestamps null: false
+    end
+  end
+
+  config.after(:each) do
+    ActiveRecord::Base.connection.drop_table :mock_table
+  end
+end
